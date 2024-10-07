057a8071463e1523020411ad9072d767a57a7446
shallinan1 <hallisky@uw.edu>
2024-10-07 13:51:31 -0700

    Parallel computations

--- dj_search/dj_search_exact_LLM.py
+++ dj_search/dj_search_exact_LLM.py
@@ -19,11 +19,14 @@ from datasets.utils.logging import disable_progress_bar
 import re
 from IPython import embed
 from typing import List
+from multiprocessing import Pool, cpu_count
 
 datasets.logging.set_verbosity_error()
 disable_progress_bar() # Disable filter progress bar
 md = MosesDetokenizer(lang='en')
 
+tokenize_func = lambda x: nltk.tokenize.casual.casual_tokenize(x)
+detokenize = lambda x: md.detokenize(x)
 
 def find_exact_match(detokenize: Callable, doc: Document, min_ngram: int, source_data: Dataset, num_cpus: int):
     hypothesis = Hypothesis(doc, min_ngram)
@@ -66,61 +69,68 @@ def find_exact_match(detokenize: Callable, doc: Document, min_ngram: int, source
     hypothesis.finished = True
     return hypothesis.export_json()
 
+def process_single_doc(t_idx, all_gens, min_ngram, source_docs):
+    outputs = []
+    for t_doc in tqdm(all_gens, leave=False, position=1):
+        tokenized_text = tokenize_func(unidecode(t_doc["text"]))
+        tgt_doc = Document(f'tgt_{t_idx}', tokenized_text)
+        if len(tgt_doc.tokens) <= min_ngram:
+            continue
+
+        output = find_exact_match(detokenize, tgt_doc, min_ngram, source_docs[t_idx], 1) # Hardcode 1 CPU here
+        t_doc.update(output)
+        outputs.append(t_doc)
+    
+    # avg_coverage = np.average([x['coverage'] for x in outputs]) if outputs else 0
+    # std = np.std([x['coverage'] for x in outputs]) if outputs else 0
+    # avg_len = np.average([x['avg_span_len'] for x in outputs]) if outputs else 0
+    # print(f'average {min_ngram}-ngram coverage: {avg_coverage:.3f}, std: {std:.3f}, average length: {avg_len}')
+
+    return outputs
+
 
 def dj_search(generation_texts_list: List[List[str]],
-              source_docs, output_file: str, min_ngram: int, 
+              source_docs, output_file: str, 
+              min_ngram: int, 
               subset: int = None, 
               num_cpus: int = 1):
+    """
+    Perform a document search by processing generated texts and finding matches in source documents.
+
+    Args:
+        generation_texts_list (List[List[str]]): A list of lists containing generated text strings to be processed.
+        source_docs: A collection of source documents to match against the generated texts.
+        output_file (str): The path to the output file where results will be saved.
+        min_ngram (int): The minimum n-gram length for considering a match.
+        subset (int, optional): The number of items to process from the generation_texts_list. Defaults to None (process all).
+        num_cpus (int, optional): The number of CPU cores to use for multiprocessing. Defaults to 1.
+
+    Returns:
+        None: The function writes the output to the specified output file.
+    """
     data = [[{"text": g} for g in generation_texts] for generation_texts in generation_texts_list]
     data = data[:subset] if subset is not None else data
-
-    tokenize_func = lambda x: nltk.tokenize.casual.casual_tokenize(x)
-    detokenize = lambda x: md.detokenize(x)
+    # embed()
 
     all_outputs = []
 
-    # if os.path.isfile(output_file):
-    #     outputs = json.load(open(output_file, 'r'))
-    #     data = data[len(outputs):]
-    #     print(f'resume from previous output file with {len(outputs)} items')
+    if num_cpus > 1:
+        print(f"Launching search in parallel with {num_cpus}")
+        func_inputs = [(t_idx, all_gens, min_ngram, source_docs) for t_idx, all_gens in enumerate(data)]
+        with Pool(num_cpus) as pool:
+            all_outputs = list(pool.starmap(process_single_doc, tqdm(func_inputs, total=len(func_inputs), position=0)))
+    else:
+        print("Launching search iteratively")
+        for t_idx, all_gens in tqdm(enumerate(data), desc='target gens', total=len(data)):         
+            outputs = process_single_doc(t_idx, all_gens, min_ngram, source_docs)
+            all_outputs.append(outputs)
+    
+    # Can't save in progress if we're using multiprocessing - too complex
+    with open(output_file, 'w') as f:
+        json.dump(all_outputs, f, indent=4)
+        f.flush()
 
-    for t_idx, all_gens in tqdm(enumerate(data), desc='target gens', total=len(data)):
-        outputs = []
-        for t_doc in all_gens:
-            tokenized_text = tokenize_func(unidecode(t_doc["text"]))
-            tgt_doc = Document(f'tgt_{t_idx}', tokenized_text)
-            if len(tgt_doc.tokens) <= min_ngram:
-                continue
-
-            output = find_exact_match(detokenize, tgt_doc, min_ngram, source_docs[t_idx], num_cpus)
-            t_doc.update(output)
-            outputs.append(t_doc)
-
-            avg_coverage = np.average([x['coverage'] for x in outputs])
-            std = np.std([x['coverage'] for x in outputs])
-            avg_len = np.average([x['avg_span_len'] for x in outputs])
-            # print(f'average {min_ngram}-ngram coverage: {avg_coverage:.3f}, std: {std:.3f}, average length: {avg_len}')
-
-        all_outputs.append(outputs)
-        with open(output_file, 'w') as f:
-            json.dump(all_outputs, f, indent=4)
-            f.flush()
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--task', type=str, default='bookMIA',
-                        help="which type of corpus to analyze")
-    parser.add_argument('--gen_data', type=str)
-    parser.add_argument('--source_docs', type=str)
-    parser.add_argument('--output_dir', type=str)
-    parser.add_argument("--min_ngram", type=int, default=3,
-                        help="minimum n-gram size")
-    parser.add_argument("--subset", type=int, default=int(1e6),
-                        help="size of example subset to run search on")
-    parser.add_argument("--generation_field", type=str, default="generation",
-                        help="Name of generations")
-
-    args = parser.parse_args()
-
+def main(args):
     # Ensure the output directory exists
     os.makedirs(args.output_dir, exist_ok=True)
 
@@ -164,14 +174,31 @@ def main():
             for g in generations:
                 cur_book_id = g["book_id"]
                 all_book_snippets = df[df["book_id"] == cur_book_id].snippet.to_list()
-                source_docs.append(Dataset.from_dict({"text": all_book_snippets}))
+                all_book_snippets_cleaned = [unidecode(x) for x in all_book_snippets]
+                source_docs.append(Dataset.from_dict({"text": all_book_snippets_cleaned}))
 
             args.output_file = args.output_file.replace(".jsonl", "_alldoc.jsonl")
 
-    dj_search(generation_texts, source_docs, args.output_file, args.min_ngram, args.subset)
+    num_workers = cpu_count() if args.parallel else 1
+    dj_search(generation_texts, source_docs, args.output_file, args.min_ngram, args.subset, num_workers)
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--task', type=str, default='bookMIA',
+                        help="which type of corpus to analyze")
+    parser.add_argument('--gen_data', type=str)
+    parser.add_argument('--source_docs', type=str)
+    parser.add_argument('--output_dir', type=str)
+    parser.add_argument("--min_ngram", type=int, default=3,
+                        help="minimum n-gram size")
+    parser.add_argument("--subset", type=int, default=int(1e6),
+                        help="size of example subset to run search on")
+    parser.add_argument("--generation_field", type=str, default="generation",
+                        help="Name of generations")
+    parser.add_argument('--parallel', action="store_true")
+
+    args = parser.parse_args()
+    main(args)
 
 
 """
@@ -179,7 +206,17 @@ python3 -m dj_search.dj_search_exact_LLM \
     --task bookMIA \
     --output_dir /gscratch/xlab/hallisky/membership-inference/tasks/bookMIA/coverages/ \
     --gen_data /gscratch/xlab/hallisky/membership-inference/tasks/bookMIA/generations/gpt-3.5-turbo-0125_maxTokens512_numSeq10_topP0.95_numSent3_startSent1_promptIdx5_len788.jsonl \
-    --min_ngram 5;
+    --min_ngram 4 \
+    --source_docs swj0419/BookMIA \
+    --parallel;
+
+python3 -m dj_search.dj_search_exact_LLM \
+    --task bookMIA \
+    --output_dir /gscratch/xlab/hallisky/membership-inference/tasks/bookMIA/coverages/ \
+    --gen_data /gscratch/xlab/hallisky/membership-inference/tasks/bookMIA/generations/gpt-3.5-turbo-0125_maxTokens512_numSeq10_topP0.95_numSent5_startSent_promptIdx5_len788.jsonl \
+    --min_ngram 4 \
+    --source_docs swj0419/BookMIA;   
+
 
 python3 -m dj_search.dj_search_exact_LLM \
     --task bookMIA \
@@ -235,6 +272,8 @@ python3 -m dj_search.dj_search_exact_LLM \
     --gen_data /gscratch/xlab/hallisky/membership-inference/tasks/bookMIA/generations/gpt-3.5-turbo-0125_maxTokens512_numSeq10_topP0.95_numSent5_startSent1_promptIdx0_len788.jsonl \
     --min_ngram 4;
 
+
+##### mini
 python3 -m dj_search.dj_search_exact_LLM \
     --task bookMIA \
     --output_dir /gscratch/xlab/hallisky/membership-inference/tasks/bookMIA/coverages/ \
@@ -271,4 +310,50 @@ python3 -m dj_search.dj_search_exact_LLM \
     --gen_data /gscratch/xlab/hallisky/membership-inference/tasks/bookMIA/generations/gpt-4o-mini-2024-07-18_maxTokens512_numSeq10_topP0.95_numSent5_startSent1_promptIdx4_len788.jsonl \
     --min_ngram 4;
 
+    
+##### Reference Model
+python3 -m dj_search.dj_search_exact_LLM \
+    --task bookMIA \
+    --output_dir /gscratch/xlab/hallisky/membership-inference/tasks/bookMIA/coverages/ \
+    --gen_data /gscratch/xlab/hallisky/membership-inference/tasks/bookMIA/generations/gpt2-large_maxTokens512_numSeq20_topP0.95_numSent3_startSent1_promptIdx0_len788_2024_10-03-00:35:53.jsonl \
+    --min_ngram 4 \
+    --parallel;
+
+
+##### 4o
+python3 -m dj_search.dj_search_exact_LLM \
+    --task bookMIA \
+    --output_dir /gscratch/xlab/hallisky/membership-inference/tasks/bookMIA/coverages/ \
+    --gen_data /gscratch/xlab/hallisky/membership-inference/tasks/bookMIA/generations/gpt-4o-2024-05-13_maxTokens512_numSeq10_topP0.95_numSent3_startSent1_promptIdx5_len788.jsonl \
+    --min_ngram 4;
+    
+python3 -m dj_search.dj_search_exact_LLM \
+    --task bookMIA \
+    --output_dir /gscratch/xlab/hallisky/membership-inference/tasks/bookMIA/coverages/ \
+    --gen_data /gscratch/xlab/hallisky/membership-inference/tasks/bookMIA/generations/gpt-4o-2024-05-13_maxTokens512_numSeq10_topP0.95_numSent5_startSent1_promptIdx5_len788.jsonl \
+    --min_ngram 4;
+
+python3 -m dj_search.dj_search_exact_LLM \
+    --task bookMIA \
+    --output_dir /gscratch/xlab/hallisky/membership-inference/tasks/bookMIA/coverages/ \
+    --gen_data /gscratch/xlab/hallisky/membership-inference/tasks/bookMIA/generations/gpt-4o-2024-05-13_maxTokens512_numSeq10_topP0.95_numSent3_startSent1_promptIdx0_len788.jsonl \
+    --min_ngram 4;
+
+python3 -m dj_search.dj_search_exact_LLM \
+    --task bookMIA \
+    --output_dir /gscratch/xlab/hallisky/membership-inference/tasks/bookMIA/coverages/ \
+    --gen_data /gscratch/xlab/hallisky/membership-inference/tasks/bookMIA/generations/gpt-4o-2024-05-13_maxTokens512_numSeq10_topP0.95_numSent5_startSent1_promptIdx0_len788.jsonl \
+    --min_ngram 4;
+
+python3 -m dj_search.dj_search_exact_LLM \
+    --task bookMIA \
+    --output_dir /gscratch/xlab/hallisky/membership-inference/tasks/bookMIA/coverages/ \
+    --gen_data /gscratch/xlab/hallisky/membership-inference/tasks/bookMIA/generations/gpt-4o-2024-05-13_maxTokens512_numSeq10_topP0.95_numSent3_startSent1_promptIdx4_len788.jsonl \
+    --min_ngram 4;
+
+python3 -m dj_search.dj_search_exact_LLM \
+    --task bookMIA \
+    --output_dir /gscratch/xlab/hallisky/membership-inference/tasks/bookMIA/coverages/ \
+    --gen_data /gscratch/xlab/hallisky/membership-inference/tasks/bookMIA/generations/gpt-4o-2024-05-13_maxTokens512_numSeq10_topP0.95_numSent5_startSent1_promptIdx4_len788.jsonl \
+    --min_ngram 4;
 """
