e850e9ee6c3f6922a65a76526b070a4ececa6f21
shallinan1 <hallisky@uw.edu>
2024-11-07 15:30:05 -0800

    Function to train all the logisitc regression omdels

---
+++ experiments/ours/train_all_logistic_regression.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Define the directories containing the .jsonl files
+DIRECTORIES=("experiments/ours/bookMIA/generations/train/")
+
+# Command to activate the environment
+ACTIVATE_ENV="source activate vllminf"
+
+# SLURM command to request resources (updated to run commands within it)
+SLURM_COMMAND="srun -p gpu-rtx6k -A xlab --time=2:00:00 --nodes=1 --cpus-per-task=1 --mem=15G --pty /bin/bash -c"
+
+# N-gram values to iterate over
+NGRAM_VALUES=(4 5 6)
+
+# Iterate over each directory
+for DIRECTORY in "${DIRECTORIES[@]}"; do
+    # Iterate over each .jsonl file in the current directory
+    for gen_path in "$DIRECTORY"*.jsonl; do
+        for ngram in "${NGRAM_VALUES[@]}"; do
+            # Create a new tmux session for each file and run the Python script
+            cmd_with_source="python3  -m experiments.ours.train_logistic_regression --gen_path $gen_path --min_ngram $ngram"
+            
+            tmux new-session -d "$SLURM_COMMAND \"$ACTIVATE_ENV && $cmd_with_source\""
+            
+            echo "Launched tmux session for $gen_path"
+        done
+    done
+done
