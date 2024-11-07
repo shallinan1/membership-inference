100e003f823cd1304f2d2ac82a3e50b3600c3d6b
shallinan1 <hallisky@uw.edu>
2024-11-06 17:10:16 -0800

    Move to utils

--- experiments/baselines/run_baselines.py
+++ experiments/baselines/run_baselines.py
@@ -14,6 +14,7 @@ from IPython import embed
 import matplotlib.pyplot as plt
 import json
 import torch
+from experiments.utils import plot_roc_curve
 
 # def inference(model1, tokenizer1,text, ex, modelname1):
 
@@ -29,21 +30,6 @@ import torch
 #     return ex
 
 
-# Function to plot ROC curve
-def plot_roc_curve(fpr, tpr, roc_auc, strategy_title, save_path):
-    plt.figure()
-    plt.plot(fpr, tpr, color='blue', lw=2, label=f'ROC curve (area = {roc_auc:0.2f})')
-    plt.plot([0, 1], [0, 1], color='red', lw=2, linestyle='--')  # Diagonal line for random guess
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title(f'{strategy_title} ROC Curve')
-    plt.grid(alpha=0.15)
-    plt.legend(loc="lower right")
-    plt.tight_layout()
-    plt.savefig(save_path, dpi=200, bbox_inches="tight")
-    plt.close()
 
 # TODO: need to get all log probs to run this method
 # def mink_pp(log_probs, all_log_probs, ratio):
