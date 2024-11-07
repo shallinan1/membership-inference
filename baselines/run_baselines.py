0be6b779cdc792e5b6b6489d131d999da4e9eb40
shallinan1 <hallisky@uw.edu>
2024-11-06 17:05:45 -0800

    Update some function args

--- baselines/run_baselines.py
+++ baselines/run_baselines.py
@@ -30,7 +30,7 @@ import torch
 
 
 # Function to plot ROC curve
-def plot_roc_curve(fpr, tpr, roc_auc, strategy_title, strategy_save, output_dir):
+def plot_roc_curve(fpr, tpr, roc_auc, strategy_title, save_path):
     plt.figure()
     plt.plot(fpr, tpr, color='blue', lw=2, label=f'ROC curve (area = {roc_auc:0.2f})')
     plt.plot([0, 1], [0, 1], color='red', lw=2, linestyle='--')  # Diagonal line for random guess
@@ -42,7 +42,7 @@ def plot_roc_curve(fpr, tpr, roc_auc, strategy_title, strategy_save, output_dir)
     plt.grid(alpha=0.15)
     plt.legend(loc="lower right")
     plt.tight_layout()
-    plt.savefig(os.path.join(output_dir, f"{strategy_save.lower()}_rocauc.png"), dpi=200, bbox_inches="tight")
+    plt.savefig(save_path, dpi=200, bbox_inches="tight")
     plt.close()
 
 # TODO: need to get all log probs to run this method
