78152c4975ed78a882791b92a23f02a93cf03902
shallinan1 <hallisky@uw.edu>
2025-01-09 14:06:46 -0800

    Reduce time

--- experiments/ours/get_all_creativity_index.sh
+++ experiments/ours/get_all_creativity_index.sh
@@ -8,17 +8,22 @@ OUTPUT_DIR="experiments/ours/bookMIA/cis"
 ACTIVATE_ENV="source activate vllminf"
 
 # SLURM command to request resources (updated to run commands within it)
-SLURM_COMMAND="srun -p gpu-rtx6k -A xlab --time=8:00:00 --nodes=1 --cpus-per-task=4 --mem=25G --pty /bin/bash -c"
+SLURM_COMMAND="srun -p gpu-rtx6k -A xlab --time=4:00:00 --nodes=1 --cpus-per-task=4 --mem=25G --pty /bin/bash -c"
 
 # Iterate over each directory
 for DIRECTORY in "${DIRECTORIES[@]}"; do
     # Iterate over each .jsonl file in the current directory
     for file_path in "$DIRECTORY"*.jsonl; do
+        # # Skip files that do not contain "turbo" in their file path
+        # if [[ "$file_path" != *"turbo"* ]]; then
+        #     continue
+        # fi
+
         # Create a new tmux session for each file and run the Python script
         cmd_with_source="python3 -m experiments.ours.get_creativity_index --coverage_path $file_path --output_dir $OUTPUT_DIR"
         
         tmux new-session -d "$SLURM_COMMAND \"$ACTIVATE_ENV && $cmd_with_source\""
         
-        echo "Launched tmux session for $file_path"
+        echo "\n$file_path"
     done
 done
