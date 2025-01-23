e2d8fb7dc9847fae5d65e146a6d2ba82d7bf3103
shallinan1 <hallisky@uw.edu>
2025-01-22 22:40:13 -0800

    update extract chunk sentence method

--- code/experiments/ours/generate.py
+++ code/experiments/ours/generate.py
@@ -14,26 +14,82 @@ from code.helper.generation.vllm_generate import ModelGenerator
 from code.helper.generation.generate_utils import task_prompts_dict_book, make_prompts
 import random
 from datetime import datetime
+from code.utils import split_text_and_newlines
 
 def extract_chunk_words(text, start_word, num_words):
     # TODO fill
     pass
 
 def extract_chunk_sentence(text, start_sentence, num_sentences):
-    text_sentences = sent_tokenize(text)
+    # Split the text by new lines and remember the spacing
+    split_texts, newline_counts = split_text_and_newlines(text)
+
+    text_sentence_count = 0
+    for cur_split_text in split_texts:
+        text_sentences = sent_tokenize(cur_split_text)
+        text_sentence_count += len(text_sentences)
+
     # Make it so we at least have the last sentence to generate
-    num_sentences = min(len(text_sentences) - start_sentence - 1, num_sentences)
-    # Ignore first one, since it is often partial
-    if len(text_sentences) == 1:
-        embed()
+    num_sentences = min(text_sentence_count - start_sentence - 1, num_sentences)
+    num_sentence_copy = num_sentences
+
+    if text_sentence_count == 1:
         try:
             prompt_text = text_sentences[start_sentence]
         except:
+            # embed()
             print("Something wrong")
             return None
-    else:
-        prompt_text = " ".join(text_sentences[start_sentence:start_sentence + num_sentences])
-        rest_of_text =  " ".join(text_sentences[start_sentence + num_sentences:])
+          
+    cur_sentence_count, total_sents = 0, 0
+    prompt_text, rest_of_text = "", ""
+
+    for i, cur_split_text in enumerate(split_texts):
+        text_sentences = sent_tokenize(cur_split_text)
+        cur_split_text_length = len(text_sentences)
+        assert num_sentences >= 0
+        assert cur_sentence_count <= num_sentence_copy
+
+        if cur_sentence_count < num_sentence_copy: # Need to build the prompt
+          if cur_sentence_count == 0: # Edge case - starting
+            if total_sents + cur_split_text_length < start_sentence + 1:
+              total_sents += cur_split_text_length
+              continue # Continue if we haven't yet reached the start idx
+            else:
+              start_idx = start_sentence - total_sents
+              assert start_idx >= 0 and start_idx < cur_split_text_length
+          else:
+            start_idx = 0
+
+          sents_to_add = min(num_sentences, cur_split_text_length - start_idx)
+          final_idx = start_idx + sents_to_add
+          assert final_idx <= cur_split_text_length
+
+          prompt_text += " ".join(text_sentences[start_idx:final_idx])
+          num_sentences -= sents_to_add
+
+          cur_sentence_count += sents_to_add
+          total_sents += cur_split_text_length
+
+          if final_idx == sents_to_add and num_sentences > 0:
+              prompt_text += newline_counts[i] * "\n"
+          else:
+            assert num_sentences == 0
+            rest_of_text += " ".join(text_sentences[final_idx:])
+            if i < len(newline_counts):
+              rest_of_text += newline_counts[i] * "\n"
+            else: 
+              assert i == len(newline_counts) 
+
+        else: # We're done building the prompt - build rest_of_text
+          rest_of_text += " ".join(text_sentences)
+          if i < len(newline_counts):
+            rest_of_text += newline_counts[i] * "\n"
+          else: 
+            assert i == len(newline_counts) 
+
+        assert num_sentences + cur_sentence_count == num_sentence_copy
+
     return prompt_text, rest_of_text
 
 # Function to define the main process
