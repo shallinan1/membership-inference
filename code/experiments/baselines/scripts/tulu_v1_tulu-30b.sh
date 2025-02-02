cd86c624b0dfe01b68a7b568c818d074ca2f527c
shallinan1 <hallisky@uw.edu>
2025-02-01 16:37:57 -0800

    Baseline scripts

--- code/experiments/baselines/scripts/tulu_v1_tulu-30b.sh
+++ code/experiments/baselines/scripts/tulu_v1_tulu-30b.sh
@@ -1,17 +1,17 @@
 python3 -m code.experiments.baselines.compute_text_probs \
-    --target_model /gscratch/xlab/hallisky/cache/tulu-13b-finalized \
+    --target_model /gscratch/xlab/hallisky/cache/tulu-30b-finalized \
     --key_name snippet \
     --task tulu_v1 \
     --split train;
 
 python3 -m code.experiments.baselines.compute_text_probs \
-    --target_model /gscratch/xlab/hallisky/cache/tulu-13b-finalized \
+    --target_model /gscratch/xlab/hallisky/cache/tulu-30b-finalized \
     --key_name snippet \
     --task tulu_v1 \
     --split val;
 
 python3 -m code.experiments.baselines.compute_text_probs \
-    --target_model /gscratch/xlab/hallisky/cache/tulu-13b-finalized \
+    --target_model /gscratch/xlab/hallisky/cache/tulu-30b-finalized \
     --key_name snippet \
     --task tulu_v1 \
     --split test;
