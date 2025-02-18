f255b0a3828403a9093442b4597273c1d26bf9a7
shallinan1 <hallisky@uw.edu>
2025-02-17 20:32:28 -0800

    Updated tulu scripts

---
+++ code/experiments/ours/scripts/tulu-13b_tulu_v1.sh
@@ -0,0 +1,23 @@
+CUDA_VISIBLE_DEVICES=0,1 python3 -m code.experiments.ours.generate \
+    --model /gscratch/xlab/hallisky/cache/tulu-13b-finalized \
+    --num_sequences 20 \
+    --max_tokens 512 \
+    --task_prompt_idx 0 \
+    --task tulu_v1 \
+    --data_split train;
+
+CUDA_VISIBLE_DEVICES=0,1 python3 -m code.experiments.ours.generate \
+    --model /gscratch/xlab/hallisky/cache/tulu-13b-finalized \
+    --num_sequences 20 \
+    --max_tokens 512 \
+    --task_prompt_idx 0 \
+    --task tulu_v1 \
+    --data_split val;
+
+CUDA_VISIBLE_DEVICES=0,1 python3 -m code.experiments.ours.generate \
+    --model /gscratch/xlab/hallisky/cache/tulu-13b-finalized \
+    --num_sequences 20 \
+    --max_tokens 512 \
+    --task_prompt_idx 0 \
+    --task tulu_v1 \
+    --data_split test;
--- code/experiments/ours/scripts/tulu-7b_tulu_v1_train.sh
+++
@@ -1,7 +0,0 @@
-python3 -m code.experiments.ours.generate \
-    --model /gscratch/xlab/hallisky/cache/tulu-7b-finalized \
-    --num_sequences 20 \
-    --max_tokens 512 \
-    --task_prompt_idx 0 \
-    --task tulu_v1 \
-    --data_split train;
