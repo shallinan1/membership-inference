734e4a84342aba2730bb10a0261041bd5de597a8
shallinan1 <hallisky@uw.edu>
2025-02-18 01:12:16 -0800

    Specify dataset

--- code/experiments/ours/scripts/run_all_scoring.sh
+++ code/experiments/ours/scripts/run_all_scoring.sh
@@ -7,18 +7,20 @@ ACTIVATE_ENV="source activate vllmgen"
 SLURM_COMMAND="srun -A cse-ckpt -p ckpt --time=1:00:00 --nodes=1 --cpus-per-task=1 --mem=25G --gres=gpu:rtx6k:0 --pty /bin/bash -c"
 
 # Define dataset and split
-DATASET="pile_external"
-SPLIT="train"
+TASK="${1:-tulu_v1}"
+SPLIT="${2:-train}"
 
 # Build paths dynamically
-TARGET_DIR="outputs/ours/${DATASET}/coverages/${SPLIT}"
+TARGET_DIR="outputs/ours/${TASK}/coverages/${SPLIT}"
+
+echo "Running"
 
 # Iterate through each file in the target directory
 for TARGET_FILE in "$TARGET_DIR"/*.jsonl; do
     cmd="python3 -m code.experiments.ours.run_scores \
         --outputs_file $TARGET_FILE"
     
-    echo "Running command: $cmd"
+    echo "$cmd;"
     echo
 
     tmux new-session -d "$SLURM_COMMAND \"$ACTIVATE_ENV && $cmd\""
