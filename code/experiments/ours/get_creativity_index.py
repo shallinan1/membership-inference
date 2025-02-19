b0faef56352ccfffff05c8247bbba554aab0f35e
shallinan1 <hallisky@uw.edu>
2025-02-18 16:49:03 -0800

    Update get creativity index function

--- code/experiments/ours/get_creativity_index.py
+++ code/experiments/ours/get_creativity_index.py
@@ -1,39 +1,33 @@
-# TODO can we use multiprocessing here?
-
-import json
 from unidecode import unidecode
 from nltk.tokenize.casual import casual_tokenize
 import numpy as np
-import numpy as np
 import os
-import itertools
+import json
 from tqdm import tqdm
-from multiprocessing import Pool, cpu_count
-from sklearn.metrics import roc_curve, auc, accuracy_score
 from IPython import embed
 import argparse
-from code.utils import load_jsonl, load_json, combine_lists, combine_dicts, combine_list_of_dicts, save_to_jsonl
+from code.utils import load_json
 import re
 
-LOW_CI_BOUND=3
+LOW_CI_BOUND=2
 HIGH_CI_BOUND=12
 
 def get_ngram_coverage(text, spans, min_gram):
-    try:
-        tokens = casual_tokenize(unidecode(text))
-    except:
-        from IPython import embed; embed()
+    tokens = casual_tokenize(unidecode(text))
     flags = [False for _ in tokens]
     for span in spans:
         span_length = span['end_index'] - span['start_index']
         if span_length >= min_gram:
             flags[span['start_index']: span['end_index']] = [True] * span_length
 
-    coverage = len([f for f in flags if f]) / len(flags)
-    return coverage
+    if len([f for f in flags if f]) == 0:
+        return 0
+    else: 
+        coverage = len([f for f in flags if f]) / len(flags)
+        return coverage
 
 def compute_ci_statistic(outputs, min_ngram, max_ngram):
-    avg_coverages, avg_std = [], []
+    total_coverages, total_stds = [], []
     ngram_list = list(range(min_ngram, max_ngram + 1))
     for output in outputs:
         coverages = []
@@ -41,63 +35,25 @@ def compute_ci_statistic(outputs, min_ngram, max_ngram):
             coverage = get_ngram_coverage(output['text'], output['matched_spans'], min_ngram)
             coverages.append(coverage)
 
-        avg_coverages.append(np.sum(coverages))
-        avg_std.append(np.std(coverages))
+        total_coverages.append(sum(coverages))
+        total_stds.append(np.std(coverages))
 
-    return avg_coverages
+    return total_coverages
 
 def main(args):
-    # Extract the directory and filename
-    directory, filename_with_date = os.path.split(args.coverage_path)
-
-    # Use a regular expression to find the date portion in the filename
-    date_pattern = r'_\d{4}-\d{2}-\d{2}-\d{2}:\d{2}:\d{2}'
-    match = re.search(date_pattern, filename_with_date)
-
-    if not match:
-        raise ValueError("Date pattern not found in the file name.")
-
-    # Extract prefix and suffix around the date portion
-    prefix = filename_with_date[:match.start()]
-    suffix = filename_with_date[match.end():]
-
-    matching_paths = []
-    # List all files in the directory and filter by matching prefix and suffix
-    for filename in sorted(os.listdir(directory)):
-        if filename.startswith(prefix) and filename.endswith(suffix):
-            # Ensure there's a date-like pattern in the filename between prefix and suffix
-            if re.search(date_pattern, filename):
-                matching_paths.append(os.path.join(directory, filename))
-
-    if args.coverage_path not in matching_paths[0]:
-        return # Don't re-do computations
-
-    combined_coverage_data = []
-
-    # Iterate over the matching paths and combine the data
-    for cur_path in matching_paths:
-        coverage_data = load_json(cur_path)
-        if not combined_coverage_data:
-            combined_coverage_data = coverage_data
-        else:
-            combined_coverage_data = combine_lists(combined_coverage_data, coverage_data)
-
-    print(len(matching_paths))
-
-    if "train" in args.coverage_path:
-        args.output_dir = os.path.join(args.output_dir, "train")
-    elif "val" in args.coverage_path:
-        args.output_dir = os.path.join(args.output_dir, "val")
-
+    data = load_json(args.coverage_path)
+    CREATIVITY_CONSTANT = args.max_ngram - args.min_ngram + 1
     os.makedirs(args.output_dir, exist_ok=True)
 
-    cis = [compute_ci_statistic(cur_data, args.min_ngram, args.max_ngram) for cur_data in tqdm(coverage_data, leave=False, desc = "Iterating through original data", position=1)]
+    for cur_data in tqdm(data, leave=False, desc = "Iterating through original data"):
+        cur_creativity = compute_ci_statistic(cur_data["coverage"], args.min_ngram, args.max_ngram)
+        cur_data["creativity"] = [CREATIVITY_CONSTANT - c for c in cur_creativity]
 
-    output_filename = os.path.join(args.output_dir, f"{filename_with_date.replace('.jsonl', '')}_CI_{args.min_ngram}_{args.max_ngram}.jsonl")
-
-#    embed()
-
-    save_to_jsonl(cis, output_filename)
+    output_file = os.path.join(args.output_dir, os.path.basename(args.coverage_path).replace('.jsonl', f"_CI{args.min_ngram}-{args.max_ngram}.jsonl"))
+    with open(output_file, 'w') as f:
+        json.dump(data, f, indent=4)
+        f.flush()
+    embed()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process JSONL file and compute statistics.")
@@ -111,6 +67,17 @@ if __name__ == "__main__":
 
     """
     python3 -m code.experiments.ours.get_creativity_index \
-    --coverage_path code/experiments/ours/outputs/bookMIA/coverages/train/Llama-2-7b-hf_maxTok512_minTok0_numSeq20_topP0.95_temp1.0_numSent1_startSent1_numWord-1_startWord-1_useSentF_promptIdx1_len494_2024-10-30-21:06:53_4_onedoc.jsonl
+    --coverage_path /gscratch/xlab/hallisky/membership-inference/outputs/ours/tulu_v1/coverages/val/tulu-7b-finalized_maxTok512_minTok0_numSeq20_topP0.95_temp1.0_numSent1_startSent-1_numWord-1_startWord-1_useSentF_promptIdx0_len92_2025-02-17-19:30:08_2_onedoc.jsonl \
+    --output_dir  /gscratch/xlab/hallisky/membership-inference/outputs/ours/tulu_v1/creativities/val/ \
+    --min_ngram 2 \
+    --max_ngram 12
+
+    python3 -m code.helper.dj_search.dj_search_exact_LLM \
+    --task bookMIA \
+    --output_dir /gscratch/xlab/hallisky/membership-inference/outputs/ours/bookMIA/coverages/train \
+    --gen_data /gscratch/xlab/hallisky/membership-inference/outputs/ours/bookMIA/generations/train/gpt-3.5-turbo-0125_maxTok512_minTok0_numSeq20_topP0.95_temp1.0_numSent5_startSent1_numWord-1_startWord-1_useSentF_promptIdx5_len494_2025-01-11-23:06:39.jsonl \
+    --min_ngram 4 \
+    --parallel \
+    --source_docs swj0419/BookMIA;
 
     """
