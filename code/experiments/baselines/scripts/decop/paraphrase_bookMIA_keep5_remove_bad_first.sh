77e5f177bc2423341e3ca61bc39da094e95bab1a
shallinan1 <hallisky@uw.edu>
2025-02-19 22:09:56 -0800

    Paraphrase for MC

---
+++ code/experiments/baselines/scripts/decop/paraphrase_bookMIA_keep5_remove_bad_first.sh
@@ -0,0 +1,35 @@
+python3 -m code.experiments.baselines.decop_paraphrase \
+    --paraphrase_model gpt-4o-2024-11-20 \
+    --task bookMIA \
+    --split val \
+    --key_name snippet \
+    --closed_model \
+    --temperature 0.1 \
+    --top_p 1 \
+    --keep_n_sentences 5 \
+    --remove_bad_first \
+    --max_tokens 3072; # 3 paraphrases - 512 tokens at most each * 2 for verbosity buffer
+
+python3 -m code.experiments.baselines.decop_paraphrase \
+    --paraphrase_model gpt-4o-2024-11-20 \
+    --task bookMIA \
+    --split train \
+    --key_name snippet \
+    --closed_model \
+    --temperature 0.1 \
+    --top_p 1 \
+    --keep_n_sentences 5 \
+    --remove_bad_first \
+    --max_tokens 3072; # 3 paraphrases - 512 tokens at most each * 2 for verbosity buffer
+
+python3 -m code.experiments.baselines.decop_paraphrase \
+    --paraphrase_model gpt-4o-2024-11-20 \
+    --task bookMIA \
+    --split test \
+    --key_name snippet \
+    --closed_model \
+    --temperature 0.1 \
+    --top_p 1 \
+    --keep_n_sentences 5 \
+    --remove_bad_first \
+    --max_tokens 3072 # 3 paraphrases - 512 tokens at most each * 2 for verbosity buffer
