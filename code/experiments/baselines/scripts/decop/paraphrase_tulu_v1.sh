8a4a9e632a805576594a2ccdb52c3574d7b6589e
shallinan1 <hallisky@uw.edu>
2025-02-21 13:02:36 -0800

    more splits

--- code/experiments/baselines/scripts/decop/paraphrase_tulu_v1.sh
+++ code/experiments/baselines/scripts/decop/paraphrase_tulu_v1.sh
@@ -6,4 +6,24 @@ python3 -m code.experiments.baselines.decop_paraphrase \
     --closed_model \
     --temperature 0.1 \
     --top_p 1 \
+    --max_tokens 2048;
+
+python3 -m code.experiments.baselines.decop_paraphrase \
+    --paraphrase_model gpt-4o-2024-11-20 \
+    --task tulu_v1 \
+    --split val \
+    --key_name snippet \
+    --closed_model \
+    --temperature 0.1 \
+    --top_p 1 \
+    --max_tokens 2048;
+
+python3 -m code.experiments.baselines.decop_paraphrase \
+    --paraphrase_model gpt-4o-2024-11-20 \
+    --task tulu_v1 \
+    --split test \
+    --key_name snippet \
+    --closed_model \
+    --temperature 0.1 \
+    --top_p 1 \
     --max_tokens 2048
