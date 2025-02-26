42a78dd1c00f2721544fada10015a15249193bb6
shallinan1 <hallisky@uw.edu>
2025-02-26 01:30:15 -0800

    Temperature ablation

--- code/experiments/ours/scripts/tulu-13b_tulu_v1.sh
+++ code/experiments/ours/scripts/tulu-13b_tulu_v1.sh
@@ -21,3 +21,30 @@ CUDA_VISIBLE_DEVICES=0,1 python3 -m code.experiments.ours.generate \
     --task_prompt_idx 0 \
     --task tulu_v1 \
     --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model /gscratch/xlab/hallisky/cache/tulu-13b-finalized \
+    --num_sequences 20 \
+    --max_tokens 512 \
+    --task_prompt_idx 0 \
+    --task tulu_v1 \
+    --temperature 0.2 \
+    --data_split train;
+
+python3 -m code.experiments.ours.generate \
+    --model /gscratch/xlab/hallisky/cache/tulu-13b-finalized \
+    --num_sequences 20 \
+    --max_tokens 512 \
+    --task_prompt_idx 0 \
+    --task tulu_v1 \
+    --temperature 0.2 \
+    --data_split val;
+
+python3 -m code.experiments.ours.generate \
+    --model /gscratch/xlab/hallisky/cache/tulu-13b-finalized \
+    --num_sequences 20 \
+    --max_tokens 512 \
+    --task_prompt_idx 0 \
+    --task tulu_v1 \
+    --temperature 0.2 \
+    --data_split test;
