6c0c60d3ab85ee63784b4d2fc8a5b19e7aa82b5b
shallinan1 <hallisky@uw.edu>
2025-02-26 01:12:20 -0800

    fix task bug bookMIA -> wikiMIA

--- code/experiments/baselines/scripts/decop/mc_wikiMIA_gpt-3.5-turbo-0125.sh
+++ code/experiments/baselines/scripts/decop/mc_wikiMIA_gpt-3.5-turbo-0125.sh
@@ -2,7 +2,7 @@ python3 -m code.experiments.baselines.decop_mc \
     --target_model gpt-3.5-turbo-0125 \
     --paraphrase_model gpt-4o-2024-11-20 \
     --key_name input \
-    --task bookMIA \
+    --task wikiMIA \
     --split val \
     --closed_model \
     --sys_prompt_idx 0;
@@ -11,7 +11,7 @@ python3 -m code.experiments.baselines.decop_mc \
     --target_model gpt-3.5-turbo-0125 \
     --paraphrase_model gpt-4o-2024-11-20 \
     --key_name input \
-    --task bookMIA \
+    --task wikiMIA \
     --split train \
     --closed_model \
     --sys_prompt_idx 0;
@@ -20,7 +20,7 @@ python3 -m code.experiments.baselines.decop_mc \
     --target_model gpt-3.5-turbo-0125 \
     --paraphrase_model gpt-4o-2024-11-20 \
     --key_name input \
-    --task bookMIA \
+    --task wikiMIA \
     --split test \
     --closed_model \
     --sys_prompt_idx 0;
