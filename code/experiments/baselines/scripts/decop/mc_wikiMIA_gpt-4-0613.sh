2cd1e814ed933688aa1f8f0c614777f6b9474802
shallinan1 <hallisky@uw.edu>
2025-02-26 16:32:06 -0800

    more wikiMIA commands

---
+++ code/experiments/baselines/scripts/decop/mc_wikiMIA_gpt-4-0613.sh
@@ -0,0 +1,26 @@
+python3 -m code.experiments.baselines.decop_mc \
+    --target_model gpt-4-0613 \
+    --paraphrase_model gpt-4o-2024-11-20 \
+    --key_name input \
+    --task wikiMIA \
+    --split val \
+    --closed_model \
+    --sys_prompt_idx 0;
+
+python3 -m code.experiments.baselines.decop_mc \
+    --target_model gpt-4-0613 \
+    --paraphrase_model gpt-4o-2024-11-20 \
+    --key_name input \
+    --task wikiMIA \
+    --split train \
+    --closed_model \
+    --sys_prompt_idx 0;
+    
+python3 -m code.experiments.baselines.decop_mc \
+    --target_model gpt-4-0613 \
+    --paraphrase_model gpt-4o-2024-11-20 \
+    --key_name input \
+    --task wikiMIA \
+    --split test \
+    --closed_model \
+    --sys_prompt_idx 0;
