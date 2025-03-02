2afc604aeaa328dd1ae11d596fd3995a1cca05ff
shallinan1 <hallisky@uw.edu>
2025-03-01 22:56:28 -0800

    Scoring and paraphrasing with no modifications on bookMIA

---
+++ code/experiments/baselines/scripts/decop/paraphrase_bookMIA_keep-1.sh
@@ -0,0 +1,29 @@
+python3 -m code.experiments.baselines.decop_paraphrase \
+    --paraphrase_model gpt-4o-2024-11-20 \
+    --task bookMIA \
+    --split val \
+    --key_name snippet \
+    --closed_model \
+    --temperature 0.1 \
+    --top_p 1 \
+    --max_tokens 3072; # 3 paraphrases - 512 tokens at most each * 2 for verbosity buffer
+
+python3 -m code.experiments.baselines.decop_paraphrase \
+    --paraphrase_model gpt-4o-2024-11-20 \
+    --task bookMIA \
+    --split train \
+    --key_name snippet \
+    --closed_model \
+    --temperature 0.1 \
+    --top_p 1 \
+    --max_tokens 3072; # 3 paraphrases - 512 tokens at most each * 2 for verbosity buffer
+
+python3 -m code.experiments.baselines.decop_paraphrase \
+    --paraphrase_model gpt-4o-2024-11-20 \
+    --task bookMIA \
+    --split test \
+    --key_name snippet \
+    --closed_model \
+    --temperature 0.1 \
+    --top_p 1 \
+    --max_tokens 3072 # 3 paraphrases - 512 tokens at most each * 2 for verbosity buffer
