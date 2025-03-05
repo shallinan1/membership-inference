d9556d1b40607e32abbe7f27a75343961bd9b9e0
shallinan1 <hallisky@uw.edu>
2025-03-05 15:16:20 -0800

    tiny update

--- code/experiments/baselines/scripts/decop/score_bookMIA_gpt-4-0613.sh
+++ code/experiments/baselines/scripts/decop/score_bookMIA_gpt-4-0613.sh
@@ -4,13 +4,13 @@ python3 -m code.experiments.baselines.decop_results \
     --split train \
     --model gpt-4-0613\
 
-    python3 -m code.experiments.baselines.decop_results \
+python3 -m code.experiments.baselines.decop_results \
     --paraphrase_model gpt-4o-2024-11-20 \
     --task bookMIA \
     --split val \
     --model gpt-4-0613
 
-    python3 -m code.experiments.baselines.decop_results \
+python3 -m code.experiments.baselines.decop_results \
     --paraphrase_model gpt-4o-2024-11-20 \
     --task bookMIA \
     --split test \
