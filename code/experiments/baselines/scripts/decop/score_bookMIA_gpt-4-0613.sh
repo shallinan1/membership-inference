998cbea34d3e464df0eed6bd072dfeb0da6ceb6b
shallinan1 <hallisky@uw.edu>
2025-03-05 15:16:06 -0800

    bookMIA scoring

--- code/experiments/baselines/scripts/decop/score_bookMIA_gpt-4-0613.sh
+++ code/experiments/baselines/scripts/decop/score_bookMIA_gpt-4-0613.sh
@@ -2,16 +2,16 @@ python3 -m code.experiments.baselines.decop_results \
     --paraphrase_model gpt-4o-2024-11-20 \
     --task bookMIA \
     --split train \
-    --model gpt-3.5-turbo-0125 \
+    --model gpt-4-0613\
 
     python3 -m code.experiments.baselines.decop_results \
     --paraphrase_model gpt-4o-2024-11-20 \
     --task bookMIA \
     --split val \
-    --model gpt-3.5-turbo-0125 
+    --model gpt-4-0613
 
     python3 -m code.experiments.baselines.decop_results \
     --paraphrase_model gpt-4o-2024-11-20 \
     --task bookMIA \
     --split test \
-    --model gpt-3.5-turbo-0125 
+    --model gpt-4-0613
