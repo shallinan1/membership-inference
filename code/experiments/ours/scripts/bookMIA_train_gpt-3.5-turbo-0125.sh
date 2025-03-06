c2681b3ae3e7bad462639b3ccf96861b5f8f6aca
shallinan1 <hallisky@uw.edu>
2025-03-06 12:18:49 -0800

    Multiple task prompt idxs

--- code/experiments/ours/scripts/bookMIA_train_gpt-3.5-turbo-0125.sh
+++ code/experiments/ours/scripts/bookMIA_train_gpt-3.5-turbo-0125.sh
@@ -5,6 +5,41 @@ python3 -m code.experiments.ours.generate \
     --num_sentences 5 \
     --num_sequences 20 \
     --max_tokens 512 \
-    --task_prompt_idx 5 4 \
+    --task_prompt_idx 4 5 \
+    --task bookMIA \
+    --data_split train;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --start_sentence 1 \
+    --num_sentences 5 \
+    --num_sequences 20 \
+    --max_tokens 512 \
+    --task_prompt_idx 4 5 \
+    --temperature 0.2 \
+    --task bookMIA \
+    --data_split train;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --start_sentence 1 \
+    --num_sentences 5 \
+    --num_sequences 10 \
+    --max_tokens 512 \
+    --task_prompt_idx 1 2 4 5 \
+    --task bookMIA \
+    --data_split train;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --start_sentence 1 \
+    --num_sentences 5 \
+    --num_sequences 10 \
+    --max_tokens 512 \
+    --task_prompt_idx 1 2 4 5 \
+    --temperature 0.2 \
     --task bookMIA \
     --data_split train;
