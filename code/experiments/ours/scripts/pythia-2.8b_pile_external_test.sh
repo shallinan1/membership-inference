c2e7be6fe4ee3c475a1af77f240921961a13010e
shallinan1 <hallisky@uw.edu>
2025-03-07 00:50:56 -0800

    One extra command

--- code/experiments/ours/scripts/pythia-2.8b_pile_external_test.sh
+++ code/experiments/ours/scripts/pythia-2.8b_pile_external_test.sh
@@ -57,3 +57,13 @@ python3 -m code.experiments.ours.generate \
     --task_prompt_idx 2 \
     --task pile_external \
     --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model EleutherAI/pythia-2.8b \
+    --start_sentence 0 \
+    --num_sentences 5 \
+    --num_sequences 10 \
+    --max_tokens 64 \
+    --task_prompt_idx 0 1 2 \
+    --task pile_external \
+    --data_split test;
