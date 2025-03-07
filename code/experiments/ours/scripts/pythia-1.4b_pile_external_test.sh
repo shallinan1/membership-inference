bd224866ba3d4ddbe9456e4ca35636150cc9499a
shallinan1 <hallisky@uw.edu>
2025-03-06 23:31:36 -0800

    More prompts

--- code/experiments/ours/scripts/pythia-1.4b_pile_external_test.sh
+++ code/experiments/ours/scripts/pythia-1.4b_pile_external_test.sh
@@ -22,8 +22,29 @@ python3 -m code.experiments.ours.generate \
     --model EleutherAI/pythia-1.4b \
     --start_sentence 0 \
     --num_sentences 5 \
-    --num_sequences 20 \
-    --max_tokens 512 \
+    --num_sequences 10 \
+    --max_tokens 128 \
     --task_prompt_idx 0 \
+    --task pile_external \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model EleutherAI/pythia-1.4b \
+    --start_sentence 0 \
+    --num_sentences 5 \
+    --num_sequences 10 \
+    --max_tokens 128 \
+    --task_prompt_idx 0 1 2 \
+    --task pile_external \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model EleutherAI/pythia-1.4b \
+    --start_sentence 0 \
+    --num_sentences 5 \
+    --num_sequences 10 \
+    --max_tokens 128 \
+    --task_prompt_idx 0 1 2 \
+    --temperature 0.2 \
     --task pile_external \
     --data_split test;
