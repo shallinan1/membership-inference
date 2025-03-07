41aaa6f7ad04a7625b0eb0292198db95c06a7d3e
shallinan1 <hallisky@uw.edu>
2025-03-07 00:50:42 -0800

    Updated Pythia commands

--- code/experiments/ours/scripts/pythia-1.4b_pile_external_test.sh
+++ code/experiments/ours/scripts/pythia-1.4b_pile_external_test.sh
@@ -48,3 +48,33 @@ python3 -m code.experiments.ours.generate \
     --temperature 0.2 \
     --task pile_external \
     --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model EleutherAI/pythia-1.4b \
+    --start_sentence 0 \
+    --num_sentences 5 \
+    --num_sequences 20 \
+    --max_tokens 64 \
+    --task_prompt_idx 0 \
+    --task pile_external \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model EleutherAI/pythia-1.4b \
+    --start_sentence 0 \
+    --num_sentences 5 \
+    --num_sequences 20 \
+    --max_tokens 64 \
+    --task_prompt_idx 1 \
+    --task pile_external \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model EleutherAI/pythia-1.4b \
+    --start_sentence 0 \
+    --num_sentences 5 \
+    --num_sequences 20 \
+    --max_tokens 64 \
+    --task_prompt_idx 2 \
+    --task pile_external \
+    --data_split test;
--- code/experiments/ours/scripts/pythia-6.9b_pile_external_test.sh
+++ code/experiments/ours/scripts/pythia-6.9b_pile_external_test.sh
@@ -27,3 +27,43 @@ python3 -m code.experiments.ours.generate \
     --task_prompt_idx 0 \
     --task pile_external \
     --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model EleutherAI/pythia-6.9b \
+    --start_sentence 0 \
+    --num_sentences 5 \
+    --num_sequences 20 \
+    --max_tokens 64 \
+    --task_prompt_idx 0 \
+    --task pile_external \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model EleutherAI/pythia-6.9b \
+    --start_sentence 0 \
+    --num_sentences 5 \
+    --num_sequences 20 \
+    --max_tokens 64 \
+    --task_prompt_idx 1 \
+    --task pile_external \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model EleutherAI/pythia-6.9b \
+    --start_sentence 0 \
+    --num_sentences 5 \
+    --num_sequences 20 \
+    --max_tokens 64 \
+    --task_prompt_idx 2 \
+    --task pile_external \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model EleutherAI/pythia-6.9b \
+    --start_sentence 0 \
+    --num_sentences 5 \
+    --num_sequences 10 \
+    --max_tokens 64 \
+    --task_prompt_idx 0 1 2 \
+    --task pile_external \
+    --data_split test;
