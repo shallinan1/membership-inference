026a31890e176cab498184916fc79a28cd10c264
shallinan1 <hallisky@uw.edu>
2025-03-09 16:29:16 -0700

    More variants

--- code/experiments/ours/scripts/wikiMIA-gpt-4-0613.sh
+++ code/experiments/ours/scripts/wikiMIA-gpt-4-0613.sh
@@ -65,3 +65,42 @@ python3 -m code.experiments.ours.generate \
     --data_split test;
 
 
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-4-0613 \
+    --start_sentence 0 \
+    --num_sentences 1 \
+    --num_sequences 20 \
+    --temperature 0.2 \
+    --max_tokens 25 \
+    --task_prompt_idx 0 \
+    --task wikiMIA \
+    --key_name input \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-4-0613 \
+    --start_sentence 0 \
+    --num_sentences 1 \
+    --num_sequences 20 \
+    --temperature 0.2 \
+    --max_tokens 25 \
+    --task_prompt_idx 1 \
+    --task wikiMIA \
+    --key_name input \
+    --data_split test;
+
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-4-0613 \
+    --start_sentence 0 \
+    --num_sentences 1 \
+    --num_sequences 20 \
+    --temperature 0.2 \
+    --max_tokens 100 \
+    --task_prompt_idx 2 \
+    --task wikiMIA \
+    --key_name input \
+    --data_split test;
