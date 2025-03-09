dc33fa234618b238536d2b872d90156a6d063735
shallinan1 <hallisky@uw.edu>
2025-03-08 19:29:57 -0800

    pile mc scripts

---
+++ code/experiments/baselines/scripts/decop/mc_pile_external_pythia-1.2b.sh
@@ -0,0 +1,23 @@
+python3 -m code.experiments.baselines.decop_mc \
+    --target_model EleutherAI/pythia-1.4b \
+    --paraphrase_model gpt-4o-2024-11-20 \
+    --key_name snippet \
+    --task pile_external \
+    --split train \
+    --sys_prompt_idx 0;
+    
+python3 -m code.experiments.baselines.decop_mc \
+    --target_model EleutherAI/pythia-1.4b \
+    --paraphrase_model gpt-4o-2024-11-20 \
+    --key_name snippet \
+    --task pile_external \
+    --split val \
+    --sys_prompt_idx 0;
+    
+python3 -m code.experiments.baselines.decop_mc \
+    --target_model EleutherAI/pythia-1.4b \
+    --paraphrase_model gpt-4o-2024-11-20 \
+    --key_name snippet \
+    --task pile_external \
+    --split test \
+    --sys_prompt_idx 0;
