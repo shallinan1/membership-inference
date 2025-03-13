4c67f6f6b92ab1318274e129d6995227641c50ad
shallinan1 <hallisky@uw.edu>
2025-03-13 01:55:55 -0700

    Couple more commands

--- code/experiments/ours/scripts/generation/wikiMIA-gpt-3.5-turbo-0125.sh
+++ code/experiments/ours/scripts/generation/wikiMIA-gpt-3.5-turbo-0125.sh
@@ -6,7 +6,7 @@ python3 -m code.experiments.ours.generate \
     --num_sequences 20 \
     --max_tokens 100 \
     --temperature 0.2 \
-    --task_prompt_idx 5 \
+    --task_prompt_idx 0 \
     --task wikiMIA \
     --key_name input \
     --data_split train;
@@ -19,7 +19,7 @@ python3 -m code.experiments.ours.generate \
     --num_sequences 20 \
     --max_tokens 100 \
     --temperature 0.2 \
-    --task_prompt_idx 5 \
+    --task_prompt_idx 0 \
     --task wikiMIA \
     --key_name input \
     --data_split val;
@@ -64,4 +64,131 @@ python3 -m code.experiments.ours.generate \
     --key_name input \
     --data_split test;
 
+
+# Use words instead
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 20 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_words_from_end 10 \
+    --data_split test;
+
+# Lower temperature 
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 20 \
+    --temperature 0.2 \
+    --task_prompt_idx 2 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_words_from_end 10 \
+    --data_split test;
+
+# Fewer sequences
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 1 \
+    --temperature 0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_words_from_end 10 \
+    --data_split test;
+
+# Don't constrain the generation to fixed tokens?
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 20 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_tokens 128 \
+    --num_words_from_end 10 \
+    --data_split test;
+
+# Less words of context
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 20 \
+    --temperature 0.2 \
+    --task_prompt_idx 2 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_words_from_end 50 \
+    --data_split test;
+
+
+# Less words of context - more generation
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 20 \
+    --temperature 0.2 \
+    --task_prompt_idx 2 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_tokens 128 \
+    --num_words_from_end 50 \
+    --data_split test;
+
+# Less words of context - higher tempreature
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 20 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_words_from_end 50 \
+    --data_split test;
+
+# Even less words of context - lower temperature
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 20 \
+    --temperature 0.2 \
+    --task_prompt_idx 2 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_words_from_end 100 \
+    --data_split test;
+
+# Even less words of context - higher temperature
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 20 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_words_from_end 100 \
+    --data_split test;
 
