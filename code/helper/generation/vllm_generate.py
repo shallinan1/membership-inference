7a42627b38b69210ad66da90bf3821d7a760a066
shallinan1 <hallisky@uw.edu>
2025-03-15 00:59:31 -0700

    support different max new tokens

--- code/helper/generation/vllm_generate.py
+++ code/helper/generation/vllm_generate.py
@@ -7,7 +7,7 @@ import json
 from IPython import embed
 from code.helper.generation.generate_utils import make_prompts
 from vllm import LLM, SamplingParams
-from typing import List, Tuple, Optional
+from typing import List, Tuple, Optional, Union
 
 class ModelGenerator:
     """
@@ -78,7 +78,7 @@ class ModelGenerator:
         temperature: float = 1.0,
         top_p: float = 0.95,
         sample: bool = True,
-        max_new_tokens: int = 256,
+        max_new_tokens: Union[int, List[int]] = 256,
         min_tokens: Optional[int] = None,
         max_length: int = 2048,
         extra_stop_tokens: Optional[List[int]] = None,
@@ -102,6 +102,9 @@ class ModelGenerator:
         Returns:
             Tuple[List[str], List[str]]: A tuple containing a list of final prompts after filtering and a list of generated outputs corresponding to those prompts.
         """
+        if isinstance(max_new_tokens, int):
+            max_new_tokens = [max_new_tokens] * len(prompts)
+        
         # Convert extra stop tokens to integers if provided
         extra_stop_tokens = [int(a) for a in extra_stop_tokens] if extra_stop_tokens else []
 
@@ -114,15 +117,15 @@ class ModelGenerator:
         truncated = 0
 
         # Filter out prompts that are too long and check for padding tokens
-        for prompt, input_ids in zip(prompts, input_ids_list):
+        for prompt, input_ids, max_new_token in zip(prompts, input_ids_list, max_new_tokens):
             assert self.tokenizer.pad_token_id not in input_ids, "Padding tokens found in input_ids."
-            if len(input_ids) + max_new_tokens <= max_length:
+            if len(input_ids) + max_new_token <= max_length:
                 filtered_input_ids.append(input_ids)
                 final_prompts.append(prompt)
             else:
                 truncated += 1
                 # print(f"Truncating from {len(input_ids)} to {max_length-max_new_tokens}")
-                shortened = self.tokenizer([prompt], truncation=True, max_length = max_length-max_new_tokens).input_ids[0]
+                shortened = self.tokenizer([prompt], truncation=True, max_length=max_length-max_new_token).input_ids[0]
                 filtered_input_ids.append(shortened)
                 final_prompts.append(self.tokenizer.decode(shortened))
         print(f"Truncated {truncated} out of {len(final_prompts)}")
@@ -132,16 +135,16 @@ class ModelGenerator:
         if not sample:
             temperature = 0  # vllm will always use sampling unless temperature = 0
 
-        sampling_params = SamplingParams(
+        sampling_params = [SamplingParams(
             temperature=temperature,
             top_p=top_p,
-            max_tokens=max_new_tokens,
+            max_tokens=mnt,
             min_tokens=min_tokens,
             stop_token_ids=stop_token_ids,
             n=n,
             logprobs=20,
             # prompt_logprobs=5
-        )
+        ) for mnt in max_new_tokens]
 
         # Generation
         output = self.llm.generate(
@@ -149,7 +152,6 @@ class ModelGenerator:
             prompt_token_ids=filtered_input_ids
         )
         
-
         all_text_outputs = []
         all_prompt_logprobs = []
         all_output_logprobs = []
