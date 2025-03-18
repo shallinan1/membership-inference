63942c7ad96ccc4ecec94b16e964e10c3863adfd
shallinan1 <hallisky@uw.edu>
2025-03-18 14:13:45 -0700

    Baseline for tulu 1.1

---
+++ code/experiments/baselines/scripts/tulu_v1.1_tulu-13b.sh
@@ -0,0 +1,17 @@
+python3 -m code.experiments.baselines.compute_text_probs \
+    --target_model allenai/tulu-v1-llama2-13b  \
+    --key_name snippet \
+    --task tulu_v1 \
+    --split train;
+
+python3 -m code.experiments.baselines.compute_text_probs \
+    --target_model allenai/tulu-v1-llama2-13b  \
+    --key_name snippet \
+    --task tulu_v1 \
+    --split val;
+    
+python3 -m code.experiments.baselines.compute_text_probs \
+    --target_model allenai/tulu-v1-llama2-13b  \
+    --key_name snippet \
+    --task tulu_v1 \
+    --split test;
