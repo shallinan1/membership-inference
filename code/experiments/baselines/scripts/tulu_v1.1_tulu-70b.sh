fc1ee87b30451a5b3cee4b832849ed5e460f0620
shallinan1 <hallisky@uw.edu>
2025-03-18 16:48:19 -0700

    update 7 -> 70b

--- code/experiments/baselines/scripts/tulu_v1.1_tulu-70b.sh
+++ code/experiments/baselines/scripts/tulu_v1.1_tulu-70b.sh
@@ -1,17 +1,17 @@
 python3 -m code.experiments.baselines.compute_text_probs \
-    --target_model allenai/tulu-v1-llama2-7b  \
+    --target_model allenai/tulu-v1-llama2-70b  \
     --key_name snippet \
     --task tulu_v1 \
     --split train;
 
 python3 -m code.experiments.baselines.compute_text_probs \
-    --target_model allenai/tulu-v1-llama2-7b  \
+    --target_model allenai/tulu-v1-llama2-70b  \
     --key_name snippet \
     --task tulu_v1 \
     --split val;
     
 python3 -m code.experiments.baselines.compute_text_probs \
-    --target_model allenai/tulu-v1-llama2-7b  \
+    --target_model allenai/tulu-v1-llama2-70b  \
     --key_name snippet \
     --task tulu_v1 \
     --split test;
