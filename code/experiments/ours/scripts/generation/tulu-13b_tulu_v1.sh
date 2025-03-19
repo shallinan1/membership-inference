cbdf748048818d7b5fe1a39afcfab280d5609a51
shallinan1 <hallisky@uw.edu>
2025-03-19 14:40:01 -0700

    to max seq length for tulu

--- code/experiments/ours/scripts/generation/tulu-13b_tulu_v1.sh
+++ code/experiments/ours/scripts/generation/tulu-13b_tulu_v1.sh
@@ -48,3 +48,13 @@ python3 -m code.experiments.ours.generate \
     --task tulu_v1 \
     --temperature 0.2 \
     --data_split test;
+
+# Max length
+python3 -m code.experiments.ours.generate \
+    --model /gscratch/xlab/hallisky/cache/tulu-13b-finalized \
+    --num_sequences 20 \
+    --max_tokens 512 \
+    --task_prompt_idx 0 \
+    --task tulu_v1 \
+    --max_length_to_sequence_length \
+    --data_split test;
--- code/experiments/ours/scripts/generation/tulu-30b_tulu_v1.sh
+++ code/experiments/ours/scripts/generation/tulu-30b_tulu_v1.sh
@@ -21,3 +21,13 @@ python3 -m code.experiments.ours.generate \
     --task_prompt_idx 0 \
     --task tulu_v1 \
     --data_split test;
+
+# Max length
+python3 -m code.experiments.ours.generate \
+    --model /gscratch/xlab/hallisky/cache/tulu-30b-finalized \
+    --num_sequences 20 \
+    --max_tokens 512 \
+    --task_prompt_idx 0 \
+    --task tulu_v1 \
+    --max_length_to_sequence_length \
+    --data_split test;
