9622cc0429201c2237a7c14de89e2cbf02faa992
shallinan1 <hallisky@uw.edu>
2025-03-20 02:34:47 -0700

    wikiMIA commands

---
+++ code/experiments/ours/scripts/generation/wikiMIA-llama-7b.sh
@@ -0,0 +1,231 @@
+python3 -m code.experiments.ours.generate \
+    --model huggyllama/llama-7b \
+    --start_sentence 0 \
+    --num_sentences 1 \
+    --num_sequences 20 \
+    --temperature 0.2 \
+    --max_tokens 100 \
+    --task_prompt_idx 0 \
+    --task wikiMIA \
+    --key_name input \
+    --data_split test;
+
+# Use words instead
+python3 -m code.experiments.ours.generate \
+    --model huggyllama/llama-7b \
+    --num_sequences 20 \
+    --temperature 1.0 \
+    --task_prompt_idx 0 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_words_from_end 10 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model huggyllama/llama-7b \
+    --num_sequences 20 \
+    --temperature 1.0 \
+    --task_prompt_idx 1 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_words_from_end 10 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model huggyllama/llama-7b \
+    --num_sequences 20 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_words_from_end 10 \
+    --data_split test;
+
+# More variants
+python3 -m code.experiments.ours.generate \
+    --model huggyllama/llama-7b \
+    --num_sequences 20 \
+    --temperature 0.2 \
+    --task_prompt_idx 2 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_words_from_end 10 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model huggyllama/llama-7b \
+    --num_sequences 20 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_tokens 100 \
+    --num_words_from_end 10 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model huggyllama/llama-7b \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_words_from_end 10 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model huggyllama/llama-7b \
+    --num_sequences 20 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_words_from_end 25 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model huggyllama/llama-7b \
+    --num_sequences 20 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_words_from_end 50 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model huggyllama/llama-7b \
+    --start_sentence 0 \
+    --num_sentences 1 \
+    --num_sequences 20 \
+    --temperature 0.2 \
+    --max_tokens 100 \
+    --task_prompt_idx 0 \
+    --task wikiMIA \
+    --key_name input \
+    --data_split test;
+
+# Use words instead
+python3 -m code.experiments.ours.generate \
+    --model huggyllama/llama-7b \
+    --num_sequences 20 \
+    --temperature 1.0 \
+    --task_prompt_idx 0 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_words_from_end 10 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model huggyllama/llama-7b \
+    --num_sequences 20 \
+    --temperature 1.0 \
+    --task_prompt_idx 1 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_words_from_end 10 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model huggyllama/llama-7b \
+    --num_sequences 20 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_words_from_end 10 \
+    --data_split test;
+
+# More variants
+python3 -m code.experiments.ours.generate \
+    --model huggyllama/llama-7b \
+    --num_sequences 20 \
+    --temperature 0.2 \
+    --task_prompt_idx 2 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_words_from_end 10 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model huggyllama/llama-7b \
+    --num_sequences 20 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_tokens 100 \
+    --num_words_from_end 10 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model huggyllama/llama-7b \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_words_from_end 10 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model huggyllama/llama-7b \
+    --num_sequences 20 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_words_from_end 25 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model huggyllama/llama-7b \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_words_from_end 50 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model huggyllama/llama-7b \
+    --num_sequences 20 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_tokens 100 \
+    --num_words_from_end 50 \
+    --data_split test;
