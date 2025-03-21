a26297701992db9a46254c88ef7171331e0f7caa
shallinan1 <hallisky@uw.edu>
2025-03-20 22:14:27 -0700

    Update plotting

--- code/plotting/plot_metrics.py
+++ code/plotting/plot_metrics.py
@@ -22,11 +22,8 @@ name_map = {
 def main(args):
     df= pd.read_csv(args.data_path, sep='\t')
 
-    exclude_cols = ['num_sequences', 'min_ngram']
-    metrics = [col for col in df.columns if col not in exclude_cols]
-
     # Columns to exclude from metrics
-    exclude_cols = ['num_sequences', 'min_ngram']
+    exclude_cols = [args.save_name, 'min_ngram']
     metrics = [col for col in df.columns if col not in exclude_cols]
     num_metrics = len(metrics)
 
@@ -43,9 +40,9 @@ def main(args):
         for ngram in sorted(df['min_ngram'].unique()):
             df_subset = df[df['min_ngram'] == ngram]
             if "Longest" in metric:
-                ax.plot(df_subset['num_sequences'], df_subset[metric], 'o--', alpha=0.7,  color="black")
+                ax.plot(df_subset[args.save_name], df_subset[metric], 'o--', alpha=0.7,  color="black")
             else:
-                ax.plot(df_subset['num_sequences'], df_subset[metric], 'o--', label=f'Min N-Gram={ngram}', alpha=0.7)
+                ax.plot(df_subset[args.save_name], df_subset[metric], 'o--', label=f'Min N-Gram={ngram}', alpha=0.7)
 
             if "Longest" in metric:
                 break
@@ -55,18 +52,21 @@ def main(args):
         cur_title = f'{metric.split("_")[1]}'
         ax.set_title(name_map.get(cur_title, cur_title))
         ax.set_xticks([10, 20, 50, 100])
+        # ax.set_xticks(np.array([10, 25, 50, 75, 90])/100)
         ax.grid(alpha=0.3)
         if i == num_metrics-3:
-            ax.legend(ncols=num_metrics+1, loc='upper center', bbox_to_anchor=(0.95, -0.19))
+            ax.legend(ncols=num_metrics+1, loc='upper center', bbox_to_anchor=(0.95, -0.21))
 
     plt.tight_layout()
-    fig.supxlabel('Number of Sequences', fontsize=14, font=bold_font, y=-0.04)
+    fig.supxlabel(args.x_label, fontsize=18, font=bold_font, y=-0.06)
     plt.savefig(f"code/plotting/plots/{args.save_name}.pdf", bbox_inches="tight")
     plt.show()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Simple script to parse data path.")
     parser.add_argument('--data_path', type=str, default="code/plotting/temp_data.tsv", help="Path to the data directory.")
+    parser.add_argument('--x_label', type=str, help="Path to the data directory.")
+
     parser.add_argument("--save_name")
     main(parser.parse_args())
 
@@ -75,4 +75,10 @@ if __name__ == "__main__":
 python3 -m code.plotting.plot_metrics \
     --data_path code/plotting/temp_data_min_num_sequences.tsv \
     --save_name num_sequences \
+    --x_label "Number of Sequences Generated"
+    
+python3 -m code.plotting.plot_metrics \
+    --data_path code/plotting/temp_data_min_proportion.tsv \
+    --save_name num_proportion_from_end \
+    --x_label "Proportion of Text as Prompt"
 """
