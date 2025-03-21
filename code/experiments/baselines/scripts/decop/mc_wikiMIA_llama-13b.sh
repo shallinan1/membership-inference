33c37fe04b0fe2be78202a0a088265144e3dbb87
shallinan1 <hallisky@uw.edu>
2025-03-21 01:28:15 -0700

    De-COP on more models

---
+++ code/experiments/baselines/scripts/decop/mc_wikiMIA_llama-13b.sh
@@ -0,0 +1,25 @@
+python3 -m code.experiments.baselines.decop_mc \
+    --target_model huggyllama/llama-13b \
+    --paraphrase_model gpt-4o-2024-11-20 \
+    --key_name input \
+    --task wikiMIA \
+    --split val \
+    --closed_model \
+    --sys_prompt_idx 0;
+
+python3 -m code.experiments.baselines.decop_mc \
+    --target_model huggyllama/llama-13b \
+    --paraphrase_model gpt-4o-2024-11-20 \
+    --key_name input \
+    --task wikiMIA \
+    --split train \
+    --closed_model \
+    --sys_prompt_idx 0;
+    
+python3 -m code.experiments.baselines.decop_mc \
+    --target_model huggyllama/llama-13b \
+    --paraphrase_model gpt-4o-2024-11-20 \
+    --key_name input \
+    --task wikiMIA \
+    --split test \
+    --sys_prompt_idx 0;
