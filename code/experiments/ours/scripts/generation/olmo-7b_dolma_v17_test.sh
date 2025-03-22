91819b70480c584b3312478d28e09f12a0cddd7d
shallinan1 <hallisky@uw.edu>
2025-03-21 17:03:42 -0700

    update olmos

--- code/experiments/ours/scripts/generation/olmo-7b_dolma_v17_test.sh
+++ code/experiments/ours/scripts/generation/olmo-7b_dolma_v17_test.sh
@@ -33,3 +33,52 @@ python3 -m code.experiments.ours.generate \
     --prompt_with_words_not_sent \
     --max_length_to_sequence_length \
     --num_words_from_end 50;
+
+
+python3 -m code.experiments.ours.generate \
+    --model allenai/OLMo-7b-0724-hf \
+    --max_tokens 512 \
+    --task_prompt_idx 1 \
+    --task dolma_v17 \
+    --data_split test \
+    --num_sequences 20 \
+    --temperature 1.0 \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.5;
+
+python3 -m code.experiments.ours.generate \
+    --model allenai/OLMo-7b-0724-hf \
+    --max_tokens 512 \
+    --task_prompt_idx 1 \
+    --task dolma_v17 \
+    --data_split test \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.5;
+
+python3 -m code.experiments.ours.generate \
+    --model allenai/OLMo-7b-0724-hf \
+    --max_tokens 512 \
+    --task_prompt_idx 1 \
+    --task dolma_v17 \
+    --data_split test \
+    --num_sequences 100 \
+    --temperature 1.0 \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.5;
+
+python3 -m code.experiments.ours.generate \
+    --model allenai/OLMo-7b-0724-hf \
+    --max_tokens 512 \
+    --task_prompt_idx 1 \
+    --task dolma_v17 \
+    --data_split test \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.75;
