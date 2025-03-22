e322d1bcf79a3e50c87009ba94b83397c092291c
shallinan1 <hallisky@uw.edu>
2025-03-21 17:01:45 -0700

    add commands

--- code/experiments/ours/scripts/generation/pythia-6.9b_pile_external_test.sh
+++ code/experiments/ours/scripts/generation/pythia-6.9b_pile_external_test.sh
@@ -67,3 +67,53 @@ python3 -m code.experiments.ours.generate \
     --task_prompt_idx 0 1 2 \
     --task pile_external \
     --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model EleutherAI/pythia-6.9b \
+    --num_sequences 20 \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.5 \
+    --task_prompt_idx 0  \
+    --task pile_external \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model EleutherAI/pythia-6.9b \
+    --num_sequences 50 \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.5 \
+    --task_prompt_idx 0  \
+    --task pile_external \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model EleutherAI/pythia-6.9b \
+    --num_sequences 20 \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.75 \
+    --task_prompt_idx 0  \
+    --task pile_external \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model EleutherAI/pythia-6.9b \
+    --num_sequences 50 \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.75 \
+    --task_prompt_idx 0  \
+    --task pile_external \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model EleutherAI/pythia-6.9b \
+    --num_sequences 50 \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.5 \
+    --task_prompt_idx 1  \
+    --task pile_external \
+    --data_split test;
