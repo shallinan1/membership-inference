94fea0007c02bafeeb4bb36a5752dc07dc51edfa
shallinan1 <hallisky@uw.edu>
2025-03-23 19:06:55 -0700

    0125

---
+++ code/experiments/ours/scripts/generation/articles_gpt-3.5-turbo-0125.sh
@@ -0,0 +1,260 @@
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 0 \
+    --task articles \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.5 \
+    --remove_bad_first \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 1 \
+    --task articles \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.5 \
+    --remove_bad_first \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task articles \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.5 \
+    --remove_bad_first \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 3 \
+    --task articles \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.5 \
+    --remove_bad_first \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 4 \
+    --task articles \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.5 \
+    --remove_bad_first \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 0 \
+    --task articles \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.75 \
+    --remove_bad_first \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 1 \
+    --task articles \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.75 \
+    --remove_bad_first \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task articles \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.75 \
+    --remove_bad_first \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 3 \
+    --task articles \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.75 \
+    --remove_bad_first \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 4 \
+    --task articles \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.75 \
+    --remove_bad_first \
+    --data_split test;
+
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 0 \
+    --task articles \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.25 \
+    --remove_bad_first \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 1 \
+    --task articles \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.25 \
+    --remove_bad_first \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task articles \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.25 \
+    --remove_bad_first \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 3 \
+    --task articles \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.25 \
+    --remove_bad_first \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 4 \
+    --task articles \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.25 \
+    --remove_bad_first \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 0 \
+    --task articles \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.1 \
+    --remove_bad_first \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task articles \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.1 \
+    --remove_bad_first \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task articles \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.1 \
+    --remove_bad_first \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 3 \
+    --task articles \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.1 \
+    --remove_bad_first \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-0125 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 4 \
+    --task articles \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.1 \
+    --remove_bad_first \
+    --data_split test;
