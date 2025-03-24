7618d546fd218343380b8f5f6bad9f3a9303bb44
shallinan1 <hallisky@uw.edu>
2025-03-23 19:06:44 -0700

    0.1 commands

--- code/experiments/ours/scripts/generation/articles_gpt-3.5-turbo-instruct.sh
+++ code/experiments/ours/scripts/generation/articles_gpt-3.5-turbo-instruct.sh
@@ -193,3 +193,68 @@ python3 -m code.experiments.ours.generate \
     --num_proportion_from_end 0.25 \
     --remove_bad_first \
     --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-instruct \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 0 \
+    --task articles \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.1 \
+    --remove_bad_first \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-instruct \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task articles \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.1 \
+    --remove_bad_first \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-instruct \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task articles \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.1 \
+    --remove_bad_first \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-instruct \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 3 \
+    --task articles \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.1 \
+    --remove_bad_first \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-instruct \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 4 \
+    --task articles \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.1 \
+    --remove_bad_first \
+    --data_split test;
