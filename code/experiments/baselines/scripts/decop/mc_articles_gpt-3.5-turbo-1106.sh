3e5d8df37c95deef092bc2b209b71ff08a4ec889
shallinan1 <hallisky@uw.edu>
2025-03-26 14:46:41 -0700

    articles data

---
+++ code/experiments/baselines/scripts/decop/mc_articles_gpt-3.5-turbo-1106.sh
@@ -0,0 +1,8 @@
+python3 -m code.experiments.baselines.decop_mc \
+    --target_model gpt-3.5-turbo-1106 \
+    --paraphrase_model gpt-4o-2024-11-20 \
+    --key_name snippet \
+    --task articles \
+    --split test \
+    --closed_model \
+    --sys_prompt_idx 0;
