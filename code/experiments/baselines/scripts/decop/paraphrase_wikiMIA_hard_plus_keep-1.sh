6aa638579a8519c760c79b635fd8ae8ba69cb3e4
shallinan1 <hallisky@uw.edu>
2025-03-27 22:20:28 -0700

    paraphrasing

---
+++ code/experiments/baselines/scripts/decop/paraphrase_wikiMIA_hard_plus_keep-1.sh
@@ -0,0 +1,29 @@
+python3 -m code.experiments.baselines.decop_paraphrase \
+    --paraphrase_model gpt-4o-2024-11-20 \
+    --task wikiMIA_hard \
+    --split val \
+    --key_name input \
+    --closed_model \
+    --temperature 0.1 \
+    --top_p 1 \
+    --max_tokens 3072; # 3 paraphrases - 512 tokens at most each * 2 for verbosity buffer
+
+python3 -m code.experiments.baselines.decop_paraphrase \
+    --paraphrase_model gpt-4o-2024-11-20 \
+    --task wikiMIA_hard \
+    --split train \
+    --key_name input \
+    --closed_model \
+    --temperature 0.1 \
+    --top_p 1 \
+    --max_tokens 3072; # 3 paraphrases - 512 tokens at most each * 2 for verbosity buffer
+
+python3 -m code.experiments.baselines.decop_paraphrase \
+    --paraphrase_model gpt-4o-2024-11-20 \
+    --task wikiMIA_hard \
+    --split test \
+    --key_name input \
+    --closed_model \
+    --temperature 0.1 \
+    --top_p 1 \
+    --max_tokens 3072 # 3 paraphrases - 512 tokens at most each * 2 for verbosity buffer
