66b8e24d8e6d034c391ffcb8f97b7a688188bf8d
shallinan1 <hallisky@uw.edu>
2025-03-28 03:32:53 -0700

    updated

--- code/experiments/ours/scripts/generation/wikiMIA-llama-7b.sh
+++ code/experiments/ours/scripts/generation/wikiMIA-llama-7b.sh
@@ -229,3 +229,52 @@ python3 -m code.experiments.ours.generate \
     --max_tokens 100 \
     --num_words_from_end 50 \
     --data_split test;
+
+# More scripts
+python3 -m code.experiments.ours.generate \
+    --model huggyllama/llama-7b \
+    --num_sequences 20 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.25 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model huggyllama/llama-7b \
+    --num_sequences 20 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.5 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model huggyllama/llama-7b \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.5 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model huggyllama/llama-7b \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 0 \
+    --task wikiMIA \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.5 \
+    --data_split test;
