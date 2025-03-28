2c940c07d63df9a8595b7a495f759a9ee7809798
shallinan1 <hallisky@uw.edu>
2025-03-27 20:58:55 -0700

    scoring

---
+++ code/experiments/baselines/scripts/decop/score_wikiMIA_hard-3.5-turbo-1106.sh
@@ -0,0 +1,5 @@
+python3 -m code.experiments.baselines.decop_results \
+    --paraphrase_model gpt-4o-2024-11-20 \
+    --task wikiMIA_hard \
+    --split test \
+    --model gpt-3.5-turbo-0125
---
+++ code/experiments/baselines/scripts/decop/score_wikiMIA_hard-gpt-4o-2024-11-20.sh
@@ -0,0 +1,6 @@
+python3 -m code.experiments.baselines.decop_results \
+    --paraphrase_model gpt-4o-2024-11-20 \
+    --task wikiMIA_hard \
+    --split test \
+    --model gpt-4o-2024-11-20
+    
