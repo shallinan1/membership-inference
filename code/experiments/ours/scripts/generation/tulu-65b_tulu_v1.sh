cea89038bf9108a7b46e9a7e55f3609ece784fb9
shallinan1 <hallisky@uw.edu>
2025-03-28 12:47:23 -0700

    tulu 50

--- code/experiments/ours/scripts/generation/tulu-65b_tulu_v1.sh
+++ code/experiments/ours/scripts/generation/tulu-65b_tulu_v1.sh
@@ -31,3 +31,11 @@ python3 -m code.experiments.ours.generate \
     --task tulu_v1 \
     --max_length_to_sequence_length \
     --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --model /gscratch/xlab/hallisky/cache/tulu-65b-finalized \
+    --num_sequences 50 \
+    --max_tokens 512 \
+    --task_prompt_idx 0 \
+    --task tulu_v1 \
+    --data_split test;
