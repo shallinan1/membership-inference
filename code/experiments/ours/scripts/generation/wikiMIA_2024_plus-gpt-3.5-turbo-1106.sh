a106c8c54bb43debd7de2ec86e648b2f75be7353
shallinan1 <hallisky@uw.edu>
2025-04-10 14:34:09 -0700

    now deperecated commands

--- code/experiments/ours/scripts/generation/wikiMIA_2024_plus-gpt-3.5-turbo-1106.sh
+++ code/experiments/ours/scripts/generation/wikiMIA_2024_plus-gpt-3.5-turbo-1106.sh
@@ -80,3 +80,56 @@ python3 -m code.experiments.ours.generate \
     --max_length_to_sequence_length \
     --num_proportion_from_end 0.5 \
     --data_split test;
+
+# More 1106
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-1106 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 0 \
+    --task wikiMIA_2024_plus \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.5 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-1106 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA_2024_plus \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.5 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-1106 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA_2024_plus \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.25 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-1106 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 0 \
+    --task wikiMIA_2024_plus \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.25 \
+    --data_split test;
--- code/experiments/ours/scripts/generation/wikiMIA_2024_plus-gpt-3.5-turbo-instruct.sh
+++ code/experiments/ours/scripts/generation/wikiMIA_2024_plus-gpt-3.5-turbo-instruct.sh
@@ -80,3 +80,69 @@ python3 -m code.experiments.ours.generate \
     --max_length_to_sequence_length \
     --num_proportion_from_end 0.5 \
     --data_split test;
+
+# More instruct
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-instruct \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 0 \
+    --task wikiMIA_2024_plus \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.5 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-instruct \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA_2024_plus \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.5 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-instruct \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA_2024_plus \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.25 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-instruct \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA_2024_plus \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_tokens 100 \
+    --num_proportion_from_end 0.5 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-3.5-turbo-instruct \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA_2024_plus \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_tokens 200 \
+    --num_proportion_from_end 0.5 \
+    --data_split test;
