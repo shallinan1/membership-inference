85a8d096bf4c7ecb427786c40111693f2d598023
shallinan1 <hallisky@uw.edu>
2025-05-29 16:07:51 -0700

    wikiMIA update llama

---
+++ code/experiments/baselines/scripts/wikiMIA_update_llama-30b.sh
@@ -0,0 +1,10 @@
+python3 -m code.experiments.baselines.compute_text_probs \
+    --target_model huggyllama/llama-30b \
+    --key_name input \
+    --task wikiMIA_update \
+    --split test;
+
+python3 -m code.experiments.baselines.run_loss_baselines \
+    --target_model_probs outputs/baselines/wikiMIA_update/test/probs/llama-30b.jsonl \
+    --ref_model_probs outputs/baselines/wikiMIA_update/test/probs/llama-7b.jsonl \
+    --key_name input;
