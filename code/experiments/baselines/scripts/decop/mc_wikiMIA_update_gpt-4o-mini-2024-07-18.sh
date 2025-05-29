6c1f7542d39e99e926e92b1b52b6fe241ce42c89
shallinan1 <hallisky@uw.edu>
2025-05-29 16:09:02 -0700

    decop wikmia update pt 1

---
+++ code/experiments/baselines/scripts/decop/mc_wikiMIA_update_gpt-4o-mini-2024-07-18.sh
@@ -0,0 +1,8 @@
+python3 -m code.experiments.baselines.decop_mc \
+    --target_model gpt-4o-mini-2024-07-18 \
+    --paraphrase_model gpt-4o-2024-11-20 \
+    --key_name input \
+    --task wikiMIA_update \
+    --split test \
+    --closed_model \
+    --sys_prompt_idx 0;
