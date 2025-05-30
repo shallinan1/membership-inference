8ae8c65385b31ac5941a950531449fc2c63e7e67
shallinan1 <hallisky@uw.edu>
2025-05-29 17:05:31 -0700

    fix faulty command

--- code/experiments/baselines/scripts/wikiMIA_llama-65b.sh
+++ code/experiments/baselines/scripts/wikiMIA_llama-65b.sh
@@ -4,8 +4,7 @@ python3 -m code.experiments.baselines.compute_text_probs \
     --task wikiMIA \
     --split test;
 
-python3 -m code.experiments.baselines.compute_text_probs \
-    --target_model huggyllama/llama-65b \
-    --key_name input \
-    --task wikiMIA \
-    --split test;
+python3 -m code.experiments.baselines.run_loss_baselines \
+    --target_model_probs outputs/baselines/wikiMIA/test/probs/llama-65b.jsonl \
+    --ref_model_probs outputs/baselines/wikiMIA/test/probs/llama-7b.jsonl \
+    --key_name input;
