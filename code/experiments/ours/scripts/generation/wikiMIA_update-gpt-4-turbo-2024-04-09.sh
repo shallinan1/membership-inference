507af841e90958cd64b0ed7558ca4757b72277e3
shallinan1 <hallisky@uw.edu>
2025-05-29 22:19:16 -0700

    wikiMIA update gpt commands

---
+++ code/experiments/ours/scripts/generation/wikiMIA_update-gpt-4-turbo-2024-04-09.sh
@@ -0,0 +1,79 @@
+# New from here
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-4-turbo-2024-04-09 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 0 \
+    --task wikiMIA_update \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.5 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-4-turbo-2024-04-09 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 0 \
+    --task wikiMIA_update \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.75 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-4-turbo-2024-04-09 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 1 \
+    --task wikiMIA_update \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.5 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-4-turbo-2024-04-09 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 1 \
+    --task wikiMIA_update \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.75 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-4-turbo-2024-04-09 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA_update \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.5 \
+    --data_split test;
+
+python3 -m code.experiments.ours.generate \
+    --openai \
+    --model gpt-4-turbo-2024-04-09 \
+    --num_sequences 50 \
+    --temperature 1.0 \
+    --task_prompt_idx 2 \
+    --task wikiMIA_update \
+    --key_name input \
+    --prompt_with_words_not_sent \
+    --max_length_to_sequence_length \
+    --num_proportion_from_end 0.75 \
+    --data_split test;
+
