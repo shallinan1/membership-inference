076260f815717d207bceecec742838dcb1fbca25
shallinan1 <hallisky@uw.edu>
2025-05-29 21:52:12 -0700

    new task

--- code/helper/generation/generate_utils.py
+++ code/helper/generation/generate_utils.py
@@ -329,6 +329,7 @@ extra commentary, formatting, or additional text.\n\nComplete the prefix: """,
 
 task_prompts_dict_book["wikiMIA_2024_plus"] = task_prompts_dict_book["wikiMIA"]
 task_prompts_dict_book["wikiMIA_hard"] = task_prompts_dict_book["wikiMIA"]
+task_prompts_dict_book["wikiMIA_update"] = task_prompts_dict_book["wikiMIA"]
 
 for task_key in task_prompts_dict_book:
     cur_task_prompts_dict_book = task_prompts_dict_book[task_key]
