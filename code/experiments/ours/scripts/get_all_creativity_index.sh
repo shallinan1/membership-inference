40138c14b9a14883f75d0dd827a81173a5b7f34a
shallinan1 <hallisky@uw.edu>
2025-05-30 01:35:49 -0700

    sbatch

--- code/experiments/ours/scripts/get_all_creativity_index.sh
+++ code/experiments/ours/scripts/get_all_creativity_index.sh
@@ -10,22 +10,28 @@ MAX_NGRAM=12
 DIRECTORY="outputs/ours/${TASK}/coverages/${SPLIT}/"
 OUTPUT_DIR="outputs/ours/${TASK}/creativities/${SPLIT}/"
 
-ACTIVATE_ENV="source activate vllmgen"
-SLURM_COMMAND="srun -A cse-ckpt -p ckpt --time=1:00:00 --nodes=1 --cpus-per-task=1 --mem=25G --gres=gpu:rtx6k:0 --pty /bin/bash -c"
-SLURM_COMMAND="srun -A xlab -p gpu-rtx6k --time=1:00:00 --nodes=1 --cpus-per-task=1 --mem=25G --gres=gpu:rtx6k:0 --pty /bin/bash -c"
-SLURM_COMMAND="srun -A xlab -p gpu-a100 --time=1:00:00 --nodes=1 --cpus-per-task=1 --mem=25G --gpus 0 --pty /bin/bash -c"
-
 # Iterate over each .jsonl file in the current directory
 for file_path in "$DIRECTORY"*.jsonl; do
     filename=$(basename "$file_path" .jsonl)
     output_file="${OUTPUT_DIR}${filename}_CI${MIN_NGRAM}-${MAX_NGRAM}.jsonl"
 
     if [ ! -f "$output_file" ]; then
-        # Create a new tmux session for each file and run the Python script
-        cmd_with_source="python3 -m code.experiments.ours.get_creativity_index --coverage_path $file_path --output_dir $OUTPUT_DIR --min_ngram $MIN_NGRAM --max_ngram $MAX_NGRAM"
+        cmd="python3 -m code.experiments.ours.get_creativity_index \
+            --coverage_path $file_path \
+            --output_dir $OUTPUT_DIR \
+            --min_ngram $MIN_NGRAM \
+            --max_ngram $MAX_NGRAM"
         
-        echo "$cmd_with_source"
+        echo "$cmd"
         echo
-        tmux new-session -d "$SLURM_COMMAND \"$ACTIVATE_ENV && $cmd_with_source\""
+
+        sbatch -A xlab -p gpu-rtx6k \
+               --time=1:00:00 \
+               --nodes=1 \
+               --cpus-per-task=1 \
+               --mem=25G \
+               --gpus=0 \
+               --job-name="creativity_${filename}" \
+               --wrap="source activate vllmgen && $cmd"
     fi
 done
